fc7de73e5457b264bcb9b792206d9ae398f908c5
liuxiaohua <lxh_aq@163.com>
2020-08-04 01:26:51 +0800

    适配版本至1.16.10.2

--- AlwaysNoCheat/Hook.cs
+++ AlwaysNoCheat/Hook.cs
@@ -10,7 +10,7 @@ namespace AlwaysNoCheat
 	/// </summary>
 	public class Hook
 	{
-		delegate ulong CMD_REG_Func(ulong a1, ulong a2, ulong a3, int level, byte f1, byte f2);
+		delegate ulong CMD_REG_Func(ulong a1, ulong a2, ulong a3, byte level, byte f1, byte f2);
 		private static IntPtr _CS_REGISTERCOMMAND_org = IntPtr.Zero;
 		/// <summary>
 		/// 新方法，修改注册的命令标识，所有指令全部增加一个无需作弊可用的flag
@@ -22,7 +22,7 @@ namespace AlwaysNoCheat
 		/// <param name="f1">指令属性flag1</param>
 		/// <param name="f2">指令属性flag2</param>
 		/// <returns></returns>
-		private static ulong _cshook_registerCommand(ulong a1, ulong a2, ulong a3, int level, byte f1, byte f2) {
+		private static ulong _cshook_registerCommand(ulong a1, ulong a2, ulong a3, byte level, byte f1, byte f2) {
 			f1 |= (byte)MCCSAPI.CommandCheatFlag.NotCheat;
 			var org = Marshal.GetDelegateForFunctionPointer(_CS_REGISTERCOMMAND_org, typeof(CMD_REG_Func)) as CMD_REG_Func;
 			return org(a1, a2, a3, level, f1, f2);
@@ -36,11 +36,14 @@ namespace AlwaysNoCheat
 				case "1.16.1.2":
 					rva = 0x00429850;
 					break;
+				case "1.16.10.2":
+					rva = 0x00429820;
+					break;
 			}
 			if (rva != 0) {
 				bool ret = api.cshook(rva,	// IDA CommandRegistry::registerCommand
-				                      Marshal.GetFunctionPointerForDelegate(cs_reghookptr),
-			           out _CS_REGISTERCOMMAND_org);
+					           Marshal.GetFunctionPointerForDelegate(cs_reghookptr),
+					           out _CS_REGISTERCOMMAND_org);
 				if (ret)
 					Console.WriteLine("作弊指令已解锁。");
 			}
