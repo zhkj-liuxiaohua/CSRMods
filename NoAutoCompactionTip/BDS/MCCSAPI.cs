8d84374c1cda894c9a9d320116c354942f76f5dc
zhkj-liuxiaohua <lxh_aq@163.com>
2021-05-19 02:31:20 +0800

    添加版本适配 去掉baselib依赖

---
+++ NoAutoCompactionTip/BDS/MCCSAPI.cs
@@ -0,0 +1,144 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： BDSNetRunner
+ * 日期: 2020/7/17
+ * 时间: 16:27
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSR
+{
+	/// <summary>
+	/// API接口定义
+	/// </summary>
+	public class MCCSAPI
+	{
+		[DllImport("kernel32.dll")]
+        private extern static IntPtr LoadLibrary(String path);
+        [DllImport("kernel32.dll")]
+        private extern static IntPtr GetProcAddress(IntPtr lib, String funcName);
+        [DllImport("kernel32.dll")]
+        private extern static bool FreeLibrary(IntPtr lib);
+
+        private readonly string mVersion;
+        /// <summary>
+        /// 插件版本
+        /// </summary>
+        public string VERSION {get{return mVersion;}}
+        private readonly bool mcommercial;
+        /// <summary>
+        /// 平台类型
+        /// </summary>
+        public bool COMMERCIAL {get{return mcommercial;}}
+
+		private IntPtr hLib;
+        public MCCSAPI(String DLLPath, string ver, bool commercial)
+        {
+        	mVersion = ver;
+        	mcommercial = commercial;
+            hLib = LoadLibrary(DLLPath);
+            if (hLib != IntPtr.Zero) {
+            	initApis();
+            }
+        }
+        ~MCCSAPI()
+        {
+            FreeLibrary(hLib);
+        }
+        //将要执行的函数转换为委托
+        private T Invoke<T>(String APIName)
+        {
+            IntPtr api = GetProcAddress(hLib, APIName);
+            if (api != IntPtr.Zero)
+	            //return (T)Marshal.GetDelegateForFunctionPointer(api, typeof(T));
+				//若.net framework版本高于4.5.1可用以下替换以上
+				return Marshal.GetDelegateForFunctionPointer<T>(api);
+            Console.WriteLine("Get Api {0} failed.", APIName);
+            return default(T);
+        }
+        
+		private delegate bool CSHOOKFUNC(int rva, IntPtr hook, out IntPtr org);
+		private CSHOOKFUNC ccshook;
+		private delegate bool CSUNHOOKFUNC(IntPtr hook, out IntPtr org);
+		private CSUNHOOKFUNC ccsunhook;
+		private delegate IntPtr DLSYMFUNC(int rva);
+		private DLSYMFUNC cdlsym;
+		private delegate bool READHARDMEMORY(int rva, byte[] odata, int size);
+		private READHARDMEMORY creadHardMemory, cwriteHardMemory;
+		
+		// 初始化所有api函数
+		void initApis()
+		{
+			ccshook = Invoke<CSHOOKFUNC>("cshook");
+			ccsunhook = Invoke<CSUNHOOKFUNC>("csunhook");
+			cdlsym = Invoke<DLSYMFUNC>("dlsym");
+			creadHardMemory = Invoke<READHARDMEMORY>("readHardMemory");
+			cwriteHardMemory = Invoke<READHARDMEMORY>("writeHardMemory");
+		}
+
+		// 底层相关
+
+		/// <summary>
+		/// 设置一个钩子
+		/// </summary>
+		/// <param name="rva">原型函数相对地址</param>
+		/// <param name="hook">新函数</param>
+		/// <param name="org">待保存原型函数的指针</param>
+		/// <returns></returns>
+		public bool cshook(int rva, IntPtr hook, out IntPtr org)
+        {
+			IntPtr sorg = IntPtr.Zero;
+            var ret = ccshook != null && ccshook(rva, hook, out sorg);
+            org = sorg;
+            return ret;
+        }
+		/// <summary>
+		/// 卸载一个钩子
+		/// </summary>
+		/// <param name="hook">待卸载的函数</param>
+		/// <param name="org">已保存了原型函数的指针</param>
+		/// <returns></returns>
+		public bool csunhook(IntPtr hook, ref IntPtr org) {
+			IntPtr sorg = org;
+			var ret = ccsunhook != null && ccsunhook(hook, out sorg);
+			org = sorg;
+			return ret;
+		}
+		/// <summary>
+		/// 取相对地址对应的实际指针
+		/// </summary>
+		/// <param name="rva"></param>
+		/// <returns></returns>
+		public IntPtr dlsym(int rva) {
+			return cdlsym != null ? cdlsym(rva) :
+				IntPtr.Zero;
+		}
+		/// <summary>
+		/// 读特定段内存硬编码
+		/// </summary>
+		/// <param name="rva">函数片段起始位置相对地址</param>
+		/// <param name="size">内存长度</param>
+		/// <returns></returns>
+		public byte[] readHardMemory(int rva, int size) {
+			byte[] x = new byte[size];
+			if (creadHardMemory != null)
+				if (creadHardMemory(rva, x, size))
+					return x;
+			return null;
+        }
+		/// <summary>
+		/// 写特定段内存硬编码
+		/// </summary>
+		/// <param name="rva">函数片段起始位置相对地址</param>
+		/// <param name="data">新数据内容</param>
+		/// <param name="size">内存长度</param>
+		/// <returns></returns>
+		public bool writeHardMemory(int rva, byte[] data, int size) {
+			return (cwriteHardMemory != null) && cwriteHardMemory(rva, data, size);
+		}
+	}
+}
--- NoAutoCompactionTip/NoAutoCompactionTip.cs
+++ NoAutoCompactionTip/NoAutoCompactionTip.cs
@@ -1,14 +1,5 @@
-﻿/*
- * 由SharpDevelop创建。
- * 用户： Admin
- * 日期: 2021/5/17
- * 时间: 20:37
- * 
- * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
- */
-using System;
+﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using CSR;
 
@@ -31,10 +22,10 @@ namespace NoAutoCompactionTip
 		public delegate void BED_LOG(int a1, int a2, long a3, int a4, int a5, long a6, int a7, long a8);
 		
 		// 隐藏输出，直接返回
-		static BED_LOG hidelog = (a1, a2, a3, a4, a5, a6, a7, a8) => {};	// Console.WriteLine("[NoAutoCompactionTip] 此处隐藏自动压缩提示。");
+		static readonly BED_LOG hidelog = (a1, a2, a3, a4, a5, a6, a7, a8) => {};	// Console.WriteLine("[NoAutoCompactionTip] 此处隐藏自动压缩提示。");
 		
 		// 压缩任务期间，输出前截断LOG，完成任务后恢复log功能
-		static TASK_DO_CALL hook_task_do_call = (a, b) => {
+		static readonly TASK_DO_CALL hook_task_do_call = (a, b) => {
 			int rva = (int)rva_bedlogs[mapi.VERSION];
 			mapi.cshook(rva, Marshal.GetFunctionPointerForDelegate(hidelog), out f_bed_log);
 			TASK_DO_CALL org = Marshal.GetDelegateForFunctionPointer<TASK_DO_CALL>(f_do_call);
@@ -48,13 +39,16 @@ namespace NoAutoCompactionTip
 		public static void init(MCCSAPI api) {
 			mapi = api;
 			
+			rva_bedlogs["1.16.220.02"] = 0x01051430;	// IDA BedrockLog::log
+			rva_docalls["1.16.220.02"] = 0xD50380;		// IDA "Running AutoCompaction..."
 			rva_bedlogs["1.16.221.01"] = 0x01051590;
 			rva_docalls["1.16.221.01"] = 0xD50510;
 			
 			int rva = (int)rva_docalls[api.VERSION];
-			if (api.cshook(rva, Marshal.GetFunctionPointerForDelegate(hook_task_do_call), out f_do_call)) {
-				Console.WriteLine("[NoAutoCompactionTip] 隐藏AutoCompaction提示已加载。适配版本：" + api.VERSION);
-			}
+			if (rva != 0)
+				if (api.cshook(rva, Marshal.GetFunctionPointerForDelegate(hook_task_do_call), out f_do_call)) {
+					Console.WriteLine("[NoAutoCompactionTip] 隐藏AutoCompaction提示已加载。适配版本：" + api.VERSION);
+				}
 		}
 	}
 }
@@ -63,29 +57,6 @@ namespace CSR
 {
 	partial class Plugin
 	{
-		private static MCCSAPI mapi = null;
-		/// <summary>
-		/// 静态api对象
-		/// </summary>
-		public static MCCSAPI api { get { return mapi; } }
-		public static int onServerStart(string pathandversion) {
-			string path = null, version = null;
-			bool commercial = false;
-			string [] pav = pathandversion.Split(',');
-			if (pav.Length > 1) {
-				path = pav[0];
-				version = pav[1];
-				commercial = (pav[pav.Length - 1] == "1");
-				mapi = new MCCSAPI(path, version, commercial);
-				if (mapi != null) {
-					onStart(mapi);
-					GC.KeepAlive(mapi);
-					return 0;
-				}
-			}
-			Console.WriteLine("Load failed.");
-			return -1;
-		}
 		
 		/// <summary>
 		/// 通用调用接口，需用户自行实现
