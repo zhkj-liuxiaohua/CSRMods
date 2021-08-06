25f43101517ce9f6b4628f4c015ef47483955162
zhkj-liuxiaohua <lxh_aq@163.com>
2021-08-06 17:48:17 +0800

    添加ws指令显形功能

---
+++ AntiWsCmd/AntiWsCmd.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： classmates
+ * 日期: 2021/8/6
+ * 时间: 15:04
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Runtime.InteropServices;
+using CSR;
+
+namespace AntiWsCmd
+{
+	/// <summary>
+	/// ws指令显形
+	/// </summary>
+	public class MyClass
+	{
+		static MCCSAPI mapi;
+		static IntPtr org;
+		
+		public delegate long ClientAutomationCommandOrigin_getName(long a, long b);
+		
+		static readonly ClientAutomationCommandOrigin_getName wsf = (a, b) =>  {
+			IntPtr func_PlayerCommandOrigin_getName = IntPtr.Zero;
+			switch(mapi.VERSION) {
+				case "1.17.10.04":	// IDA PlayerCommandOrigin::getName
+					func_PlayerCommandOrigin_getName = mapi.dlsym(0x00786150);
+					break;
+			}
+			if (func_PlayerCommandOrigin_getName != IntPtr.Zero) {
+				ClientAutomationCommandOrigin_getName pgname = Marshal.GetDelegateForFunctionPointer<ClientAutomationCommandOrigin_getName>(
+				func_PlayerCommandOrigin_getName);
+				return pgname(a, b);
+			}
+			return 0;
+		};
+		
+		public static void init(MCCSAPI api) {
+			mapi = api;
+			switch (api.VERSION) {
+				case "1.17.10.04":	// IDA ClientAutomationCommandOrigin::getName
+					if (api.cshook(0x00786020, Marshal.GetFunctionPointerForDelegate(wsf), out org)) {
+						Console.WriteLine("[AntiWsCmd] ws指令显形已加载。");
+					}
+					break;
+			}
+		}
+	}
+}
+
+namespace CSR {
+	partial class Plugin {
+		public static void onStart(MCCSAPI api) {
+			AntiWsCmd.MyClass.init(api);
+		}
+	}
+}
---
+++ AntiWsCmd/BDS/Plugin.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： BDSNetRunner
+ * 日期: 2020/7/18
+ * 时间: 12:32
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+
+namespace CSR
+{
+	partial class Plugin
+	{
+		private static MCCSAPI mapi = null;
+		/// <summary>
+		/// 静态api对象
+		/// </summary>
+		public static MCCSAPI api { get { return mapi; } }
+		#region 插件统一调用接口，请勿随意更改
+		public static int onServerStart(string pathandversion) {
+			string path = null, version = null;
+			bool commercial = false;
+			string [] pav = pathandversion.Split(',');
+			if (pav.Length > 1) {
+				path = pav[0];
+				version = pav[1];
+				commercial = (pav[pav.Length - 1] == "1");
+				mapi = new MCCSAPI(path, version, commercial);
+				if (mapi != null) {
+					onStart(mapi);
+					GC.KeepAlive(mapi);
+					return 0;
+				}
+			}
+			Console.WriteLine("Load failed.");
+			return -1;
+		}
+		#endregion
+
+		~Plugin()
+        {
+			//Console.WriteLine("[CSR Plugin] Ref released.");
+        }
+
+		#region 必要接口 onStart ，由用户实现
+		// public static void onStart(MCCSAPI api)
+		#endregion
+	}
+}
